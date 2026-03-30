62044dfc41c20e716b6a565813c1b98ec9fbcbef
Oussema Nijewi <oussemasn@gmail.com>
2026-03-30 19:22:55 -0400

    initial entry point to be updated if needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// System
+#include "system/Queues.h"
+#include "system/SystemState.h"
+#include "system/TaskManager.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(921600); // initializes serial communication
-  Serial.println("Hello from the setup");
+  // Initialize shared resources
+  initQueues();
+  initSystemState();
+
+  // Create all FreeRTOS tasks
+  createTasks();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Hello from the loop");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  // Idle loop (FreeRTOS handles everything)
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
-
-// put function definitions here:
